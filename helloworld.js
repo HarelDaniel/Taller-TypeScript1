49aaad1e693f25568b5f345f1bbc50551b1ec3dc
Harel Daniel Neira <h.neira@uniandes.edu.co>
2024-04-24 21:30:51 -0500

    Taller Script1

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+"use strict";
+let message = 'Hello World';
+console.log(message);
+document.getElementsByTagName("h1")[0].innerHTML = "Hola desde Typescript";
---
+++ helloworld.ts
@@ -0,0 +1,3 @@
+let message: string = 'Hello World';
+console.log(message);
+document.getElementsByTagName("h1")[0].innerHTML = "Hola desde Typescript";
